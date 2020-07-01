ecbc2d8abbc1e6ce820effb3b8bae1eed5a28fc0
jhangeerhussain <jhangeer.zahrsss@gmail.com>
2020-07-01 05:17:40 -0700

    Initialize project using Create React App

---
+++ src/App2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App2 extends React.Component{
+    render(){
+        return (React.createElement("h2" , null , "Here , I create React Component with Class using createElement:")
+        )
+    }
+
+}
+
+export default App2;
